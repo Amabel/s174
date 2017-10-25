94d6cc555a6d8b36b3d17663a5890a23094e78fd
Amabel <luoweibinb@gmail.com>
2017-10-25 16:38:16 +0900

    rewrite loginservlet

--- src/main/java/univ/waseda/weibin/proppatterns/controller/LoginServlet.java
+++ src/main/java/univ/waseda/weibin/proppatterns/controller/LoginServlet.java
@@ -1,6 +1,7 @@
 package univ.waseda.weibin.proppatterns.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,49 +11,32 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.google.gson.Gson;
-
-/**
- * Servlet implementation class LoginServlet
- */
 public class LoginServlet extends HttpServlet {
-	Logger logger = LogManager.getLogger();
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * 
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private static final long serialVersionUID = -1355429612341976581L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-		doPost(request, response);
+		super.doGet(req, resp);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Gson gson = new Gson();
-		String pwd = request.getParameter("pwdInput");
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Logger logger = LogManager.getLogger();
+		String pwd = req.getParameter("pwdInput");
 //		logger.debug(pwdJson);
 //		String pwd = gson.fromJson(pwdJson, String.class);
 		logger.debug(pwd);
 		if (pwd.equalsIgnoreCase(DigestUtils.sha1Hex("dntowaseda"))) {
-			logger.debug("login succeeded redirect to: " + request.getContextPath() + "/index.html");
+			logger.debug("login succeeded redirect to: " + req.getContextPath() + "/index.html");
 //			response.sendRedirect(request.getContextPath() + "/index.html");
-			request.getRequestDispatcher("/WEB-INF/index.html").forward(request, response);
+			req.getRequestDispatcher("/WEB-INF/index.html").forward(req, resp);
 		} else {
-			request.getRequestDispatcher("/WEB-INF/login.html").forward(request, response);
+			req.getRequestDispatcher("/WEB-INF/login.html").forward(req, resp);
 		}
-		
 	}
-
 }
